bcf4ffff3c060f93583b2b62bd7d6e6f7cfeddcf
Daniel Antonio Silva <danielsilvatsi@outlook.com>
2021-04-06 22:23:15 -0400

    feat: rodando a primeira migrations

---
+++ Aula2/Aula2/Migrations/20210407022126_Primeiro Migrations.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Aula2.Migrations
+{
+    public partial class PrimeiroMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "carro",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Marca = table.Column<string>(type: "text", nullable: true),
+                    Modelo = table.Column<string>(type: "text", nullable: true),
+                    combustivel = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_carro", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "carro");
+        }
+    }
+}
